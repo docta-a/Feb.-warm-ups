383a1141c263e373d4c67ede5be5a02d313e4336
Game110AM <Game110AM@605-110-STU20>
2017-03-02 09:39:19 -0700

    hi

--- moster genarator/moster genarator/spooky.cpp
+++ moster genarator/moster genarator/spooky.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 #include <ctime>
-#include <>
+#include <cstdlib>
 using namespace std;
 
-
+//declation
 	void spooky();
 int main() {
 
 	srand(time(NULL));
 	while (true) {
-		spooky();
+		spooky();//function call
+		system("color 4D");
 		system ("pause");
+		
 	}
 }
+/////////defintions
 void spooky() {
-	int num = rand() % 6
+	int num = rand() % 6;
 		switch (num){
 		case 0:
-			cout << "You got Levy in the morning!" << endl;
+			cout << "You got Jaime with her Gucci Choclates!" << endl;
 			break;
 		case 1:
-			cout << "You got Silent bro without a hair cut!" << endl;
+			cout << "You got Silent bro in the morning!" << endl;
 			break;
 		case 2:
-			cout << "You got Levy with a haircut!" << endl;
+			cout << "You got Levy with a haircut! Legend says if you look at his hair you turn into stone!" << endl;
 			break;
 		case 3:
-			cout << "You got Chutulu!" << endl;
+			cout << "You got Chutulu! the god of destruction!" << endl;
 			break;
 		case 4:
 			cout << "You got Mewtwo!" << endl;
 			break;
 		case 5:
-			cout << "You got yourself!" << endl;
+			cout << "You got yourself! The worst of all monstars" << endl;
 			break;
 
 		}
