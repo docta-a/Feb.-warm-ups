930f8aa03a93a867bbc465eee46208fc75aca6fd
Alexis Sanchez <wane20142015@gmail.com>
2017-03-08 09:49:33 -0700

    hihihihhh

--- super dooper game time/super dooper game time/game time.cpp
+++ super dooper game time/super dooper game time/game time.cpp
@@ -3,27 +3,28 @@
 using namespace std;
 int main() {
 	char input;
-	int room;
+	int room= 0;
 
-	while (input)
+	while (input = 'q')
 		switch (room) {
-		case 1:
+		case 0:
 			cout << "you are in room 1! you can go East(e)";
 			cin >> input;
 			room = 1;
 			if (input == 'e')
-				cout << "go on to the next room" << endl;
+				cout << "You have enterd the room with no windows, but 3 doors" << endl;
 			break;
 			//////////////room number 2
-		case 2:	
-			cout << "you are in room 2! you can go South(s),West (w),North";
+		case 1:	
+			cout << "you can go South(s),West (w),North (n)";
 			cin >> input;
 			room = 2;
 			if (input == 's')
-				cout << "go on to the next room" << endl;
+			if (input == 'w')
+			if (input == 'n')
 			break;
 			////////////////////////room number 3
-		case 3:
+		case 2:
 			cout << "you are in room 3! you can go South(s) or West (w)";
 			cin >> input;
 			room = 3;
